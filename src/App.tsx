5f268cee8d831427b0c923443c0a78348890bc21
manteleannkatherine@outlook.com <manteleannkatherine@outlook.com>
2022-02-07 13:17:15 +0400

    setup components

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import CurrencyInput from './components/CurrencyInput';
+import {useState,useEffect} from "react";
+import axios from 'axios';
 
 function App() {
+
+  const [amountFrom, setAmountFrom] = useState(1);
+  const [amountTo, setAmountTo] = useState(1);
+  const [currencyFrom, setCurrencyFrom] = useState('USD');
+  const [currencyTo, setCurrencyTo] = useState('USD');
+
+  const [rates, setRates] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://github.com/fawazahmed0/currency-api/tree/1/2020-11-30')
+      .then(res => { 
+        try {
+          setRates(res.data.rates);
+
+          console.log(rates)
+        }
+        catch {
+          console.log("Exceeded API call limit")
+        }
+      }
+    )}, [rates]);
+
+  const handleAmountChange = (amountFrom : any) => {
+    setAmountTo(amountFrom * rates[0] / rates[1]);
+    setAmountFrom(amountFrom)
+  }
+
+  const handleCurrencyChange = (currency : any) => {
+    setAmountTo(amountFrom * rates[0] / rates[1]);
+    setCurrencyFrom(currency);
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <CurrencyInput 
+        currencies={Object.keys(rates)} 
+        amount={amountFrom} 
+        currency={currencyFrom}
+      />
+      <CurrencyInput 
+        currencies={Object.keys(rates)} 
+        amount={amountTo} 
+        currency={currencyTo}
+      />
     </div>
   );
 }
---
+++ src/components/CurrencyInput.tsx
@@ -0,0 +1,40 @@
+import {useState} from 'react';
+import PropTypes from 'prop-types';
+
+CurrencyInput.propTypes = {
+    amount: PropTypes.number.isRequired,
+    currency : PropTypes.string.isRequired,
+    currencies: PropTypes.array
+}
+
+function CurrencyInput(props : any) {
+
+    const [amountValue, setAmountValue] = useState(1);
+    const [currencyValue, setCurrencyValue] = useState([]);
+
+    const handleAmountChange = (e : any) => {
+        setAmountValue(e.target.value);
+    }
+
+    const handleCurrencyChange = (e : any) => {
+        setCurrencyValue(e.target.value);
+    }
+    
+    return (
+        <div className="group">
+            <input 
+                type="text" 
+                value={amountValue ?? props.amount} 
+                onChange={(e) => handleAmountChange(e)} />
+            <select 
+                value={currencyValue ?? props.currency} 
+                onChange={(e) => handleCurrencyChange(e)}>
+                {props.currencies.map((currency : any) => (
+                    <option value={currency}>{currency}</option>
+                ))}
+            </select>
+        </div>
+    );
+}
+
+export default CurrencyInput;
